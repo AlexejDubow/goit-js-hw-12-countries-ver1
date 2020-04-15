e424562c5269042ee2411aafa2716a9c8e3f4f66
Alexej <alexejdubow@gmail.com>
2020-04-16 00:13:41 +0300

    hw-12 done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,40 +1,35 @@
-import PNotify from 'pnotify/dist/es/PNotify';
-import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
-import 'pnotify/dist/PNotifyBrightTheme.css';
 
 const baseUrl = 'https://restcountries.eu/rest/v2/name/';
-export default function fetchCountries(searchQuery) {
-  fetch(baseUrl + searchQuery)
-    .then(response => {
-      // console.log(response.json())
-      return response.json();
-    })
-    .then(data => {
-      let dataListCountry;
-      // console.log(data.value);
-      if (data.length > 10) {
-        PNotify.error({
-          title: '',
-          text: 'Too many matches found. Please enter a more specific query!',
-          hide: false,
-        });
-      } else if (data.length < 10 && data.length > 2) {
-        PNotify.closeAll();
-        dataListCountry = data.map(item => item.name);
-        console.log(dataListCountry);
 
-        // return dataListCountry;
-        // return dataListCountry
-        // console.log(dataListCountry);
-      } else {
-        dataListCountry = data.map(item => item.name);
-        // console.log(dataListCountry);
+export default {
+  fetchCountries(query) {
+    return fetch(baseUrl + query).then(response => response.json());
+  },
+};
 
-        return dataListCountry;
-        // return dataListCountry
+// inputQuery.addEventListener('input', debounce(showInput, 500));
 
-        // console.log(dataListCountry)
-      }
-    });
-}
-// console.log(dataListCountry);
+// function showInput() {
+//   fetch(baseUrl + inputQuery.value)
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       let dataListCountry;
+//       if (data.length > 10) {
+//         PNotify.error({
+//           title: '',
+//           text: 'Too many matches found. Please enter a more specific query!',
+//           hide: false,
+//         });
+//       } else if (data.length < 10 && data.length > 2) {
+//         PNotify.closeAll();
+//         dataListCountry = data.map(item => item.name);
+//         console.log(dataListCountry);
+//         buildMenu(dataListCountry);
+//       } else {
+//         dataListCountry = data.map(item => item.name);
+//       }
+//     });
+// }
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,63 @@
-
-import markupTemplate from './template.hbs';
 import fetchCountries from './fetchCountries';
+import PNotify from 'pnotify/dist/es/PNotify';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+import countryListTemplate from './templates/countryList.hbs';
+import countryInfoTemplate from './templates/countryInfo.hbs';
+
 import debounce from 'lodash.debounce';
 import './styles.css';
 
-const inputQuery = document.querySelector('.js-input');
+const refs = {
+  queryInput: document.querySelector('.js-input'),
+  markupCountryList: document.querySelector('.js-countryList'),
+};
+refs.queryInput.addEventListener('input', debounce(serchCountry, 500));
 
-inputQuery.addEventListener('input', debounce(showInput, 500));
+function serchCountry(e) {
+  const input = e.target.value;
+  if (input) {
+    fetchCountries.fetchCountries(input).then(data => {
+      if (data.length > 10) {
+        pnotifyClose();
+        pnotifyError();
+        clearMarkup();
+      } else if (data.length < 10 && data.length > 2) {
+        pnotifyClose();
+        clearMarkup();
+        const countryName = data.map(item => item.name);
+        const markupCountryName = buildCountryList(countryName);
+        refs.markupCountryList.insertAdjacentHTML(
+          'beforeend',
+          markupCountryName,
+        );
+      } else {
+        pnotifyClose();
+        clearMarkup();
+        buildCountryInfo(data);
+      }
+    });
+  }
+}
 
-function showInput() {
-  console.log(inputQuery.value)
-   const countries = fetchCountries(inputQuery.value);
-  console.log(countries);
-  return countries
-  // buildMenu(countries)
-  // console.log(fetchCountries);
+function buildCountryList(items) {
+  return countryListTemplate(items);
 }
-function buildMenu(menu) {
-  const markupMenu = menu.map(item => markupTemplate(item)).join('');
-  inputQuery.insertAdjacentHTML('beforeend', markupMenu);
+function buildCountryInfo(items) {
+  const markupCountryInfo = countryInfoTemplate(items);
+  refs.markupCountryList.insertAdjacentHTML('beforeend', markupCountryInfo);
 }
-// buildMenu(countries)
+
+function clearMarkup() {
+  refs.markupCountryList.innerHTML = '';
+}
+function pnotifyError() {
+  PNotify.error({
+    title: '',
+    text: 'Too many matches found. Please enter a more specific query!',
+    hide: false,
+  });
+}
+
+function pnotifyClose() {
+  PNotify.closeAll();
+}
